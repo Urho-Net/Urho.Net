dc73836de9969b99194a5f73a7acfcd9564b0345
Eli <elix22@gmail.com>
2022-09-13 08:37:17 +0300

    [dotnet-6.x] Fix Windows UrhoCooker Android build process

--- script/create-project.sh
+++ script/create-project.sh
@@ -175,6 +175,16 @@ cp "-r" "tools/ReferenceAssemblyResolver" "$projPath/tools"
 cp "-R" "tools/bash_mini/." "$projPath/tools/bash"
 cp "-r" "tools/UrhoCooker" "$projPath/tools"
 
+cp "template/obfuscar.xml" "$projPath/obfuscar.xml"
+
+mkdir "-p" "$projPath/platform"
+mkdir "-p" "$projPath/platform/android/java"
+mkdir "-p" "$projPath/platform/ios"
+mkdir "-p" "$projPath/platform/windows"
+mkdir "-p" "$projPath/platform/linux"
+mkdir "-p" "$projPath/platform/macos"
+mkdir "-p" "$projPath/platform/web"
+
 echo "Successful creation"
 echo "Project name : '$projName'"
 echo "Unique identifier : '$uuid'"
