f3859d227bb9bf530b3b36ff88b0e223d43c42a9
Eli Aloni <aloni.eli@gmail.com>
2025-04-02 11:15:45 +0300

    .NET9 NativeAOT , verify that Android NDK & SDK are installed and that  is set  to the path of the Android NDK , and  is set to the path of the Android SDK

--- tools/UrhoCooker/obj/Release/net6.0/UrhoCooker.AssemblyInfo.cs
+++ tools/UrhoCooker/obj/Release/net6.0/UrhoCooker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UrhoCooker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12bc48098c5f1af700638d7c55f21c36a41603bd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a94afce77b64b643622ed982473cad8e665cab0a")]
 [assembly: System.Reflection.AssemblyProductAttribute("UrhoCooker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UrhoCooker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
